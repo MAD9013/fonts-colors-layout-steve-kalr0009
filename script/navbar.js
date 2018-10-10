0951d5ea76e5f98a8ab35024d08296eb34b6ee7f
kalr0009 <43047778+kalr0009@users.noreply.github.com>
2018-10-10 13:45:36 -0400

    Fixed Navbar

--- script/navbar.js
+++ script/navbar.js
@@ -1,17 +1,13 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
+window.onscroll = function() {navScroll()};
 var navbar = document.getElementsByClassName("mainnav")[0];
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    navbar.classList.add("sticky");
+var offset = navbar.offsetTop;
+
+function navScroll() {
+  if (window.pageYOffset > offset) {
+    navbar.classList.add("navScroll");
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove("navScroll");
   }
 } 
